2129d3acb2d18b472ee4deddeb14da62609eeb84
uyakaps <100175433+uyakaps@users.noreply.github.com>
2022-10-01 19:14:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['628979174144']
+global.ownernomer = "628979174144"
+global.premium = ['628979174144']
+global.packname = 'Sticker  Rika'
+global.author = 'Rika Chan'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
